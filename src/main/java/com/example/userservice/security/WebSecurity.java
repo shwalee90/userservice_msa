5dcc9482aa4c33c791c15ea0835942c4320c91f9
DESKTOP-ROAF007\shwalee <qnfrgmfr16@gmail.com>
2022-06-01 22:26:21 +0900

    security 적용 (1)

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.IP_ADDRESS;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -13,8 +15,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .access("hasIpAddress('" + IP_ADDRESS + "')")             //  IP_ADDRESS="x.x.x.x"
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        try {
+            authenticationFilter.setAuthenticationManager(authenticationManager());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return authenticationFilter;
     }
 }
